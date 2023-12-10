d007c666e77074a09f006e64edd34eeea0a3d7b6
catacoman1 <catalin.coman@student.upt.ro>
2023-12-10 14:40:26 +0200

    first commit

---
+++ demo 6/src/test/java/com/citydangeralert/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citydangeralert.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
