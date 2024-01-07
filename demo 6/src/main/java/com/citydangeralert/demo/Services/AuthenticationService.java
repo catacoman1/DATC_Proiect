16d91aeca8852b114ffdf04fed8864d247270484
SuleaDragos <suleadragos@gmail.com>
2024-01-08 00:29:29 +0200

    Bug-Fix: Request not getting names and points

--- demo 6/src/main/java/com/citydangeralert/demo/Services/AuthenticationService.java
+++ demo 6/src/main/java/com/citydangeralert/demo/Services/AuthenticationService.java
@@ -26,11 +26,13 @@ public class AuthenticationService {
 
     public AuthenticationResponse register(RegisterRequest request) {
         var user = Users.builder()
-                .firstName(request.getFirstname())
-                .lastName(request.getLastname())
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
                 .email(request.getEmail())
                 .password(passwordEncoder.encode(request.getPassword()))
                 .role(request.getRole())
+                .points(0)
+                .OrderIndex(9)
                 .build();
         repository.save(user);
 
